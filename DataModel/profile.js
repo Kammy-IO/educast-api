a059be27aece4ea618d15a89c6e1451f8e5f3e87
Harikrishnan Narasiman <Harikrish.narasiman@gmail.com>
2020-07-11 12:37:33 -0400

    New data model

---
+++ DataModel/profile.js
@@ -0,0 +1,26 @@
+let profiles = [{
+    name: {
+        first_name: 'Harikrish',
+        last_name: 'Narasiman'
+    },
+    grade: 1,
+    email_id: 'Harikrish@ahaeducation.in',
+    favorite_category: [
+        'Science',
+        'Arts',
+        'Technology'
+    ],
+    mobile_number: 8624382823,
+    country_code: 91,
+    active: true,
+    courses_enrolled:[{
+        course_id: '',
+        class_ids: [
+
+        ],
+        course_title: '',
+        institute_name: '',
+        teacher_name: '',
+        delivery_method: '',
+    }]
+}]
